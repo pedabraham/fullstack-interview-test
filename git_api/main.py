7e5380c82a8684f0fc3c11209fff618360a9e171
Pedro A. Moreno <pedabraham@gmail.com>
2022-03-04 09:12:44 -0600

    add CORS

--- git_api/main.py
+++ git_api/main.py
@@ -5,9 +5,24 @@ from sqlalchemy.orm import Session
 from typing import Optional
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Dependency
 def get_db():
     db = SessionLocal()
