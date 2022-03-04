9b0c89b624c68e83061dfe2976402ce9e500e581
Pedro A. Moreno <pedabraham@gmail.com>
2022-03-04 09:12:19 -0600

    add pr endpointd

---
+++ git_api/main.py
@@ -0,0 +1,44 @@
+from ssl import OP_NO_COMPRESSION
+from fastapi import Depends, FastAPI, HTTPException, Response
+from sqlalchemy.orm import Session
+
+from typing import Optional
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/pr/", response_model=schemas.PR)
+def create_pr(pr: schemas.PRCreate, db: Session = Depends(get_db)):
+    print('hello world')
+    print(pr)
+    return crud.create_pr(db=db, pr=pr)
+
+
+@app.get("/pr/", response_model=list[schemas.PR])
+def read_prs(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    prs = crud.get_prs(db, skip=skip, limit=limit)
+    return prs
+
+
+@app.get("/pr/{pr_id}", response_model=schemas.PR)
+def read_pr(pr_id: int, db: Session = Depends(get_db)):
+    db_pr = crud.get_pr(db, pr_id=pr_id)
+    if db_pr is None:
+        raise HTTPException(status_code=404, detail="PR not found")
+    return db_pr
+
+
+@app.put("/pr/{pr_id}", response_model=schemas.PR)
+def update_pr(pr_id: int, status: str, db: Session = Depends(get_db)):
+    return crud.update_pr(db=db, id=pr_id, new_status=status)
+
